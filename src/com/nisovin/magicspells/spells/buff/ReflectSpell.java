d84b6cb6656c81b4645ccbeafcbbed6499b95e47
TheComputerGeek2 <Sam1010D@gmail.com>
2015-09-18 12:48:55 -0700

    Added 2 new config options to ReflectSpell. 'reflected-spell-power-multiplier' is multiplied by the spell power of the reflected spell before it is turned against the attacker. 'spell-power-affects-reflected-power' determines if the power that the reflect spell was cast with should be multiplied by the reflected spell power multiplier.

--- src/com/nisovin/magicspells/spells/buff/ReflectSpell.java
+++ src/com/nisovin/magicspells/spells/buff/ReflectSpell.java
@@ -1,5 +1,6 @@
 package com.nisovin.magicspells.spells.buff;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 import org.bukkit.entity.Player;
@@ -11,23 +12,27 @@ import com.nisovin.magicspells.util.MagicConfig;
 
 public class ReflectSpell extends BuffSpell {
 
-	private HashSet<String> reflectors;
+	private HashMap<String, Float> reflectors;
 	private HashSet<String> shieldBreakerNames;
 	
 	String strShieldBrokenSelf;
 	String strShieldBrokenTarget;
+	float reflectedSpellPowerMultiplier;
+	boolean spellPowerAffectsReflectedPower;
 	
 	public ReflectSpell(MagicConfig config, String spellName) {
 		super(config, spellName);
 		
-		reflectors = new HashSet<String>();
+		reflectors = new HashMap<String, Float>();
 		shieldBreakerNames = new HashSet<String>();
 		shieldBreakerNames.addAll(getConfigStringList("shield-breakers", null));
+		reflectedSpellPowerMultiplier = (float) getConfigDouble("reflected-spell-power-multiplier", 1.0);
+		spellPowerAffectsReflectedPower = getConfigBoolean("spell-power-affects-reflected-power", false);
 	}
 	
 	@Override
 	public boolean castBuff(Player player, float power, String[] args) {
-		reflectors.add(player.getName());
+		reflectors.put(player.getName(), power);
 		return true;
 	}
 
@@ -37,6 +42,7 @@ public class ReflectSpell extends BuffSpell {
 		if (event.getTarget() instanceof Player) {
 			Player target = (Player)event.getTarget();
 			if (isActive(target)) {
+				float power = reflectors.get(target.getName());
 				if (shieldBreakerNames.contains(event.getSpell().getInternalName())) {
 					turnOffBuff(target);
 					return;
@@ -44,6 +50,7 @@ public class ReflectSpell extends BuffSpell {
 				boolean ok = chargeUseCost(target);
 				if (ok) {
 					event.setTarget(event.getCaster());
+					event.setPower(event.getPower()* reflectedSpellPowerMultiplier * (spellPowerAffectsReflectedPower ? power : 1));
 					addUse(target);
 				}
 			}
@@ -62,7 +69,7 @@ public class ReflectSpell extends BuffSpell {
 
 	@Override
 	public boolean isActive(Player player) {
-		return reflectors.contains(player.getName());
+		return reflectors.containsKey(player.getName());
 	}
 
 }
