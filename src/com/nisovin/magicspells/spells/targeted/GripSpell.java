e22f87d07e18f7d089f57625bc0e4f922fb8f60d
Chronoken <norwinofficial@gmail.com>
2018-01-02 19:53:05 +0100

    Updated GripSpell:     relative-offset accepts vector value (x, y, z) - modifies the teleport location     added trail/line effect position support for all spell casts

--- src/com/nisovin/magicspells/spells/targeted/GripSpell.java
+++ src/com/nisovin/magicspells/spells/targeted/GripSpell.java
@@ -1,62 +1,56 @@
 package com.nisovin.magicspells.spells.targeted;
 
 import org.bukkit.Location;
-import org.bukkit.entity.LivingEntity;
+import org.bukkit.util.Vector;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.LivingEntity;
 
+import com.nisovin.magicspells.util.BlockUtils;
+import com.nisovin.magicspells.util.TargetInfo;
+import com.nisovin.magicspells.util.MagicConfig;
+import com.nisovin.magicspells.spells.TargetedSpell;
+import com.nisovin.magicspells.spells.TargetedEntitySpell;
 import com.nisovin.magicspells.spelleffects.EffectPosition;
 import com.nisovin.magicspells.spells.TargetedEntityFromLocationSpell;
-import com.nisovin.magicspells.spells.TargetedEntitySpell;
-import com.nisovin.magicspells.spells.TargetedSpell;
-import com.nisovin.magicspells.util.BlockUtils;
-import com.nisovin.magicspells.util.MagicConfig;
-import com.nisovin.magicspells.util.TargetInfo;
-import org.bukkit.util.Vector;
 
 public class GripSpell extends TargetedSpell implements TargetedEntitySpell, TargetedEntityFromLocationSpell {
 
 	float locationOffset;
 	float yOffset;
-	
+	Vector relativeOffset;
 	String strCantGrip;
-	
+
 	public GripSpell(MagicConfig config, String spellName) {
 		super(config, spellName);
-		
+
+		yOffset = getConfigFloat("y-offset", 0);
 		locationOffset = getConfigFloat("location-offset", 0);
-		yOffset = getConfigFloat("y-offset", 0.5F);
+		relativeOffset = getConfigVector("relative-offset", "0,0.5,0");
+
+		if (locationOffset != 0) relativeOffset.setX(locationOffset);
+		if (yOffset != 0) relativeOffset.setY(yOffset);
+
 		strCantGrip = getConfigString("str-cant-grip", "");
 	}
 
 	@Override
 	public PostCastAction castSpell(Player player, SpellCastState state, float power, String[] args) {
 		if (state == SpellCastState.NORMAL) {
+
 			TargetInfo<LivingEntity> target = getTargetedEntity(player, power);
-			if (target != null) {
-				boolean ok = grip(player, target.getTarget());
-				if (!ok) return noTarget(player, strCantGrip);
-				sendMessages(player, target.getTarget());
-				return PostCastAction.NO_MESSAGES;
-			}
-			return noTarget(player);
+			if (target == null) return noTarget(player);
+			if (!grip(player.getLocation(), target.getTarget())) return noTarget(player, strCantGrip);
+			sendMessages(player, target.getTarget());
+			return PostCastAction.NO_MESSAGES;
+
 		}
 		return PostCastAction.HANDLE_NORMALLY;
-	}
-	
-	private boolean grip(Player player, LivingEntity target) {
-		Location loc = player.getLocation();
-		Vector v = loc.getDirection().setY(0).normalize().multiply(locationOffset);
-		loc = loc.add(v);
-		loc.add(0, yOffset, 0);
-		if (!BlockUtils.isSafeToStand(loc)) return false;
-		playSpellEffects(player, target);
-		return target.teleport(loc);
 	}
 
 	@Override
 	public boolean castAtEntity(Player caster, LivingEntity target, float power) {
 		if (!validTargetList.canTarget(caster, target)) return false;
-		return grip(caster, target);
+		return grip(caster.getLocation(), target);
 	}
 
 	@Override
@@ -71,22 +65,25 @@ public class GripSpell extends TargetedSpell implements TargetedEntitySpell, Tar
 
 	@Override
 	public boolean castAtEntityFromLocation(Location from, LivingEntity target, float power) {
-		if (validTargetList.canTarget(target)) {
-			Location loc = from.clone();
-			loc.setX(loc.getBlockX() + .5);
-			loc.setY(loc.getBlockY() + .5);
-			loc.setZ(loc.getBlockZ() + .5);
-			if (!BlockUtils.isSafeToStand(loc)) {
-				loc.add(0, 1, 0);
-				if (!BlockUtils.isSafeToStand(loc)) return false;
-			}
-			Location start = target.getLocation();
-			playSpellEffects(EffectPosition.TARGET, target);
-			target.teleport(loc);
-			playSpellEffectsTrail(start, loc);
-			return true;
-		}
-		return false;
+		if (!validTargetList.canTarget(target)) return false;
+		return grip(from, target);
+	}
+
+	private boolean grip(Location from, LivingEntity target) {
+		Location loc = from.clone();
+
+		Vector startDir = loc.clone().getDirection().normalize();
+		Vector horizOffset = new Vector(-startDir.getZ(), 0.0, startDir.getX()).normalize();
+		loc.add(horizOffset.multiply(relativeOffset.getZ())).getBlock().getLocation();
+		loc.add(loc.getDirection().clone().multiply(relativeOffset.getX()));
+		loc.setY(loc.getY() + relativeOffset.getY());
+
+		if (!BlockUtils.isPathable(loc.getBlock())) return false;
+
+		playSpellEffects(EffectPosition.TARGET, target);
+		playSpellEffectsTrail(from, loc);
+
+		return target.teleport(loc);
 	}
 
 }
