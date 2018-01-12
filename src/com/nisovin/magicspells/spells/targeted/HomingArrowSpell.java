346ece9e47dacb3b9934996dc29ba9310cd7f05b
TheComputerGeek2 <Sam1010D@gmail.com>
2018-01-12 15:24:44 -0800

    Remove outdated logic from HomingArrowSpell

--- src/com/nisovin/magicspells/spells/targeted/HomingArrowSpell.java
+++ src/com/nisovin/magicspells/spells/targeted/HomingArrowSpell.java
@@ -25,8 +25,6 @@ public class HomingArrowSpell extends TargetedSpell implements TargetedEntitySpe
 
 	float velocity;
 	int specialEffectInterval = 0;
-	Class<? extends Projectile> projectileType = Snowball.class;
-	String projectileClassName = "org.bukkit.entity.Snowball";
 	
 	List<HomingArrow> arrows = new ArrayList<>();
 	int monitor = 0;
@@ -38,20 +36,6 @@ public class HomingArrowSpell extends TargetedSpell implements TargetedEntitySpe
 		
 		velocity = getConfigFloat("velocity", 1F);
 		specialEffectInterval = getConfigInt("special-effect-interval", 0);
-		projectileClassName = getConfigString("projectile-class", "org.bukkit.entity.Snowball");
-		try {
-			Class<? extends Projectile> configProjectileClass = Class.forName(projectileClassName).asSubclass(Projectile.class);
-			if (!configProjectileClass.equals(Projectile.class)) {
-				MagicSpells.error("projectile-class must be a subclass of Projectile");
-			} else {
-				projectileType = configProjectileClass;
-			}
-		} catch (ClassNotFoundException e) {
-			MagicSpells.error("Couldn't find class \"" + projectileClassName + '\"');
-		} catch (ClassCastException e) {
-			MagicSpells.error("Couldn't cast \"" + projectileClassName + "\" as Projectile");
-		}
-		
 		projectileManager = ProjectileManagers.getManager(getConfigString("projectile-type",  "arrow"));
 	}
 
