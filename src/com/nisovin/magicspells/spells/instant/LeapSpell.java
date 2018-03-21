4a321e379717fa3a018c588eae3a1535234a41a4
Chronoken <norwinofficial@gmail.com>
2018-03-21 21:36:22 +0100

    Improved LeapSpell.
    Added - rotation option (float) which rotates the leap direction by the defined amount in degrees.

--- src/com/nisovin/magicspells/spells/instant/LeapSpell.java
+++ src/com/nisovin/magicspells/spells/instant/LeapSpell.java
@@ -1,48 +1,53 @@
 package com.nisovin.magicspells.spells.instant;
 
+import java.util.Set;
 import java.util.HashSet;
 
-import com.nisovin.magicspells.Subspell;
+import org.bukkit.util.Vector;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import com.nisovin.magicspells.Subspell;
+import com.nisovin.magicspells.util.Util;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.util.Vector;
 
 import com.nisovin.magicspells.MagicSpells;
-import com.nisovin.magicspells.spelleffects.EffectPosition;
-import com.nisovin.magicspells.spells.InstantSpell;
 import com.nisovin.magicspells.util.MagicConfig;
+import com.nisovin.magicspells.spells.InstantSpell;
+import com.nisovin.magicspells.spelleffects.EffectPosition;
 
 public class LeapSpell extends InstantSpell {
 
+	private float rotation;
 	private double forwardVelocity;
 	private double upwardVelocity;
 	private boolean cancelDamage;
 	private boolean clientOnly;
 	private Subspell landSpell;
+	private String landSpellName;
 
-	private HashSet<Player> jumping = new HashSet<>();
+	private Set<Player> jumping;
 
 	public LeapSpell(MagicConfig config, String spellName) {
 		super(config, spellName);
 
+		this.jumping = new HashSet();
+
+		this.rotation = getConfigFloat("rotation", 0F);
 		this.forwardVelocity = getConfigInt("forward-velocity", 40) / 10D;
 		this.upwardVelocity = getConfigInt("upward-velocity", 15) / 10D;
 		this.cancelDamage = getConfigBoolean("cancel-damage", true);
 		this.clientOnly = getConfigBoolean("client-only", false);
-
-		String landSpellName = getConfigString("land-spell", null);
-
-		if (landSpellName != null) landSpell = new Subspell(landSpellName);
+		this.landSpellName = getConfigString("land-spell", "");
 	}
 
 	@Override
 	public void initialize() {
 		super.initialize();
-		if (landSpell == null) return;
+
+		landSpell = new Subspell(landSpellName);
 		if (!landSpell.process()) {
+			if (!landSpellName.isEmpty()) MagicSpells.error("Leap Spell '" + internalName + "' has an invalid land-spell defined!");
 			landSpell = null;
-			MagicSpells.error("Invalid land-spell for " + this.getInternalName() + " defined");
 		}
 	}
 
@@ -54,13 +59,14 @@ public class LeapSpell extends InstantSpell {
 	public PostCastAction castSpell(Player player, SpellCastState state, float power, String[] args) {
 		if (state == SpellCastState.NORMAL) {
 			Vector v = player.getLocation().getDirection();
-			v.setY(0).normalize().multiply(this.forwardVelocity * power).setY(this.upwardVelocity * power);
-			if (this.clientOnly) {
+			v.setY(0).normalize().multiply(forwardVelocity * power).setY(upwardVelocity * power);
+			if (rotation != 0) Util.rotateVector(v, rotation);
+			if (clientOnly) {
 				MagicSpells.getVolatileCodeHandler().setClientVelocity(player, v);
 			} else {
 				player.setVelocity(v);
 			}
-			this.jumping.add(player);
+			jumping.add(player);
 			playSpellEffects(EffectPosition.CASTER, player);
 		}
 
@@ -71,7 +77,7 @@ public class LeapSpell extends InstantSpell {
     public void onEntityDamage(EntityDamageEvent e) {
         if (e.getCause() != EntityDamageEvent.DamageCause.FALL || !(e.getEntity() instanceof Player)) return;
         Player pl = (Player)e.getEntity();
-        if (this.jumping.isEmpty()) return;
+        if (jumping.isEmpty()) return;
         if (!jumping.remove(pl)) return;
         if (landSpell != null) landSpell.cast(pl, 1);
         playSpellEffects(EffectPosition.TARGET, pl.getLocation());
