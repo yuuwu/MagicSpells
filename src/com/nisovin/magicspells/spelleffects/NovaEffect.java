5c9a8b82395ff0bff93742a33dcd7263832e6ce6
Chronoken <norwinofficial@gmail.com>
2018-07-14 16:17:05 +0200

    Improved NovaEffect.
    Added:
        - remove-previous-blocks (boolean) (requires an expanding nova effect) whether it should remove the previous wave of blocks
        - start-radius (int) the start radius of the first wave of blocks
        - height-per-tick (int) changes the height of the next wave of blocks by the set value (positive numbers increase the height, negative decrease it)
        - circle-shape (boolean) whether the shape of the blocks should be a circle (to form a square set it to false, to form a circle set it to true)
    Changed:
        - no longer spawns stone items at the block position

--- src/com/nisovin/magicspells/spelleffects/NovaEffect.java
+++ src/com/nisovin/magicspells/spelleffects/NovaEffect.java
@@ -1,94 +1,50 @@
 package com.nisovin.magicspells.spelleffects;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+import java.util.List;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.util.Vector;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.block.BlockFace;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
-import com.nisovin.magicspells.DebugHandler;
+import com.nisovin.magicspells.util.Util;
 import com.nisovin.magicspells.MagicSpells;
+import com.nisovin.magicspells.DebugHandler;
+import com.nisovin.magicspells.util.SpellAnimation;
 import com.nisovin.magicspells.materials.MagicMaterial;
 import com.nisovin.magicspells.materials.MagicUnknownMaterial;
-import com.nisovin.magicspells.util.BlockUtils;
-import com.nisovin.magicspells.util.SpellAnimation;
-import com.nisovin.magicspells.util.Util;
 
-/**
- * NovaEffect<br>
- * <table border=1>
- *     <tr>
- *         <th>
- *             Config Field
- *         </th>
- *         <th>
- *             Data Type
- *         </th>
- *         <th>
- *             Description
- *         </th>
- *     </tr>
- *     <tr>
- *         <td>
- *             <code>type</code>
- *         </td>
- *         <td>
- *             String
- *         </td>
- *         <td>
- *             ???
- *         </td>
- *     </tr>
- *     <tr>
- *         <td>
- *             <code>radius</code>
- *         </td>
- *         <td>
- *             Integer
- *         </td>
- *         <td>
- *             ???
- *         </td>
- *     </tr>
- *     <tr>
- *         <td>
- *             <code>expand-interval</code>
- *         </td>
- *         <td>
- *             Integer
- *         </td>
- *         <td>
- *             ???
- *         </td>
- *     </tr>
- * </table>
- */
 public class NovaEffect extends SpellEffect {
 	
 	MagicMaterial mat;
 	
 	int radius = 3;
-	
 	int novaTickInterval = 5;
 	int expandingRadiusChange = 1;
+	int startRadius = 0;
+	int heightPerTick = 0;
 	
 	double range = 20;
-
+	
+	boolean circleShape = false;
+	boolean removePreviousBlocks = true;
+	
 	@Override
 	public void loadFromString(String string) {
 		if (string != null && !string.isEmpty()) {
+			
 			String[] params = string.split(" ");
 			int type = 51;
 			byte data = 0;
+			
 			if (params.length >= 1) {
 				try {
 					type = Integer.parseInt(params[0]);
@@ -96,6 +52,7 @@ public class NovaEffect extends SpellEffect {
 					DebugHandler.debugNumberFormat(e);
 				}
 			}
+			
 			if (params.length >= 2) {
 				try {
 					data = Byte.parseByte(params[1]);
@@ -103,7 +60,9 @@ public class NovaEffect extends SpellEffect {
 					DebugHandler.debugNumberFormat(e);
 				}
 			}
+			
 			mat = new MagicUnknownMaterial(type, data);
+			
 			if (params.length >= 3) {
 				try {
 					radius = Integer.parseInt(params[2]);
@@ -111,6 +70,7 @@ public class NovaEffect extends SpellEffect {
 					DebugHandler.debugNumberFormat(e);
 				}
 			}
+			
 			if (params.length >= 4) {
 				try {
 					novaTickInterval = Integer.parseInt(params[3]);
@@ -120,26 +80,28 @@ public class NovaEffect extends SpellEffect {
 			}
 		}
 	}
-
+	
 	@Override
 	public void loadFromConfig(ConfigurationSection config) {
 		mat = MagicSpells.getItemNameResolver().resolveBlock(config.getString("type", "fire"));
 		radius = config.getInt("radius", radius);
+		startRadius = config.getInt("start-radius", startRadius);
 		novaTickInterval = config.getInt("expand-interval", novaTickInterval);
 		expandingRadiusChange = config.getInt("expanding-radius-change", expandingRadiusChange);
 		if (expandingRadiusChange < 1) expandingRadiusChange = 1;
 		
 		range = Math.max(config.getDouble("range", range), 1);
+		removePreviousBlocks = config.getBoolean("remove-previous-blocks", removePreviousBlocks);
+		heightPerTick = config.getInt("height-per-tick", heightPerTick);
+		circleShape = config.getBoolean("circle-shape", circleShape);
 	}
-
+	
 	@Override
 	public Runnable playEffectLocation(Location location) {
 		if (mat == null) return null;
 		
 		// Get nearby players
-		Item item = location.getWorld().dropItem(location, new ItemStack(Material.STONE, 0));
-		List<Entity> nearbyEntities = item.getNearbyEntities(range, range, range);
-		item.remove();
+		Collection<Entity> nearbyEntities = location.getWorld().getNearbyEntities(location, range, range, range);
 		List<Player> nearby = new ArrayList<>();
 		for (Entity e : nearbyEntities) {
 			if (!(e instanceof Player)) continue;
@@ -147,75 +109,189 @@ public class NovaEffect extends SpellEffect {
 		}
 		
 		// Start animation
-		Block b = location.getBlock();
-		if (!BlockUtils.isPathable(b)) b = b.getRelative(BlockFace.UP);
-		new NovaAnimation(nearby, location.getBlock(), mat, radius, novaTickInterval, expandingRadiusChange);
+		if (!circleShape) new NovaAnimationSquare(nearby, location.getBlock(), mat, radius, novaTickInterval, expandingRadiusChange);
+		else new NovaAnimationCircle(nearby, location.getBlock(), mat, radius, novaTickInterval, expandingRadiusChange);
 		return null;
 	}
 	
-
-	private class NovaAnimation extends SpellAnimation {
+	private class NovaAnimationSquare extends SpellAnimation {
 		
 		List<Player> nearby;
+		Set<Block> blocks;
 		Block center;
 		MagicMaterial matNova;
 		int radiusNova;
-		Set<Block> blocks;
 		int radiusChange;
 		
-		public NovaAnimation(List<Player> nearby, Block center, MagicMaterial mat, int radius, int tickInterval, int activeRadiusChange) {
+		public NovaAnimationSquare(List<Player> nearby, Block center, MagicMaterial mat, int radius, int tickInterval, int activeRadiusChange) {
 			super(tickInterval, true);
 			this.nearby = nearby;
 			this.center = center;
 			this.matNova = mat;
 			this.radiusNova = radius;
-			blocks = new HashSet<>();
-			radiusChange = activeRadiusChange;
+			this.blocks = new HashSet<>();
+			this.radiusChange = activeRadiusChange;
 		}
-
+		
 		@Override
 		protected void onTick(int tick) {
-			// Remove old fire blocks
+			tick += startRadius;
 			tick *= radiusChange;
-			for (Block block : blocks) {
-				for (Player p : nearby) {
-					Util.restoreFakeBlockChange(p, block);
-				}
-			}
-			blocks.clear();
 			
-			if (tick <= radiusNova) {
-				// Set next ring on fire
-				int bx = center.getX();
-				int y = center.getY();
-				int bz = center.getZ();
-				for (int x = bx - tick; x <= bx + tick; x++) {
-					for (int z = bz - tick; z <= bz + tick; z++) {
-						if (Math.abs(x-bx) == tick || Math.abs(z - bz) == tick) {
-							Block b = center.getWorld().getBlockAt(x, y, z);
-							if (b.getType() == Material.AIR || b.getType() == Material.LONG_GRASS) {
-								Block under = b.getRelative(BlockFace.DOWN);
-								if (under.getType() == Material.AIR || under.getType() == Material.LONG_GRASS) b = under;
-								for (Player p : nearby) {
-									Util.sendFakeBlockChange(p, b, matNova);
-								}
-								blocks.add(b);
-							} else if (b.getRelative(BlockFace.UP).getType() == Material.AIR || b.getRelative(BlockFace.UP).getType() == Material.LONG_GRASS) {
-								b = b.getRelative(BlockFace.UP);
-								for (Player p : nearby) {
-									Util.sendFakeBlockChange(p, b, matNova);
-								}
-								blocks.add(b);
-							}
-						}
-					}
+			// Remove old blocks
+			if (removePreviousBlocks) {
+				for (Block b : blocks) {
+					for (Player p : nearby) Util.restoreFakeBlockChange(p, b);
 				}
-			} else if (tick > radiusNova + 1) {
-				// Stop if done
+				blocks.clear();
+			}
+			
+			if (tick > radiusNova + 1) {
 				stop();
+				return;
+			} else if (tick > radiusNova) {
+				return;
+			}
+			
+			// Set next ring
+			int bx = center.getX();
+			int y = center.getY();
+			int bz = center.getZ();
+			y += tick * heightPerTick;
+			
+			for (int x = bx - tick; x <= bx + tick; x++) {
+				for (int z = bz - tick; z <= bz + tick; z++) {
+					if (Math.abs(x - bx) != tick && Math.abs(z - bz) != tick) continue;
+					
+					Block b = center.getWorld().getBlockAt(x, y, z);
+					if (b.getType() == Material.AIR || b.getType() == Material.LONG_GRASS) {
+						Block under = b.getRelative(BlockFace.DOWN);
+						if (under.getType() == Material.AIR || under.getType() == Material.LONG_GRASS) b = under;
+					} else if (b.getRelative(BlockFace.UP).getType() == Material.AIR || b.getRelative(BlockFace.UP).getType() == Material.LONG_GRASS) {
+						b = b.getRelative(BlockFace.UP);
+					}
+					
+					if (b.getType() != Material.AIR && b.getType() != Material.LONG_GRASS) continue;
+					
+					if (blocks.contains(b)) continue;
+					for (Player p : nearby) Util.sendFakeBlockChange(p, b, matNova);
+					blocks.add(b);
+				}
 			}
 		}
 		
+		@Override
+		protected void stop() {
+			super.stop();
+			for (Block b : blocks) {
+				for (Player p : nearby) Util.restoreFakeBlockChange(p, b);
+			}
+			blocks.clear();
+		}
+		
 	}
-
+	
+	private class NovaAnimationCircle extends SpellAnimation {
+		
+		List<Player> nearby;
+		Set<Block> blocks;
+		Block center;
+		MagicMaterial matNova;
+		int radiusNova;
+		int radiusChange;
+		
+		public NovaAnimationCircle(List<Player> nearby, Block center, MagicMaterial mat, int radius, int tickInterval, int activeRadiusChange) {
+			super(tickInterval, true);
+			this.nearby = nearby;
+			this.center = center;
+			this.matNova = mat;
+			this.radiusNova = radius;
+			this.blocks = new HashSet<>();
+			this.radiusChange = activeRadiusChange;
+		}
+		
+		@Override
+		protected void onTick(int tick) {
+			tick += startRadius;
+			tick *= radiusChange;
+			
+			// Remove old blocks
+			if (removePreviousBlocks) {
+				for (Block b : blocks) {
+					for (Player p : nearby) {
+						Util.restoreFakeBlockChange(p, b);
+					}
+				}
+				blocks.clear();
+			}
+			
+			if (tick > radiusNova + 1) {
+				stop();
+				return;
+			} else if (tick > radiusNova) {
+				return;
+			}
+			
+			// Generate the bottom block
+			Location centerLocation = center.getLocation().clone();
+			centerLocation.add(0.5, tick * heightPerTick, 0.5);
+			Block b;
+			
+			if (startRadius == 0 && tick == 0) {
+				b = centerLocation.getWorld().getBlockAt(centerLocation);
+				
+				if (b.getType() == Material.AIR || b.getType() == Material.LONG_GRASS) {
+					Block under = b.getRelative(BlockFace.DOWN);
+					if (under.getType() == Material.AIR || under.getType() == Material.LONG_GRASS) b = under;
+				} else if (b.getRelative(BlockFace.UP).getType() == Material.AIR || b.getRelative(BlockFace.UP).getType() == Material.LONG_GRASS) {
+					b = b.getRelative(BlockFace.UP);
+				}
+				
+				if (b.getType() != Material.AIR && b.getType() != Material.LONG_GRASS) return;
+				
+				if (blocks.contains(b)) return;
+				for (Player p : nearby) Util.sendFakeBlockChange(p, b, matNova);
+				blocks.add(b);
+			}
+			
+			// Generate the circle
+			Vector v;
+			double angle, x, z;
+			double amount = tick * 64;
+			double inc = (2 * Math.PI) / amount;
+			for (int i = 0; i < amount; i++) {
+				angle = i * inc;
+				x = tick * Math.cos(angle);
+				z = tick * Math.sin(angle);
+				v = new Vector(x, 0, z);
+				b = center.getWorld().getBlockAt(centerLocation.add(v));
+				centerLocation.subtract(v);
+				
+				if (b.getType() == Material.AIR || b.getType() == Material.LONG_GRASS) {
+					Block under = b.getRelative(BlockFace.DOWN);
+					if (under.getType() == Material.AIR || under.getType() == Material.LONG_GRASS) b = under;
+				} else if (b.getRelative(BlockFace.UP).getType() == Material.AIR || b.getRelative(BlockFace.UP).getType() == Material.LONG_GRASS) {
+					b = b.getRelative(BlockFace.UP);
+				}
+				
+				if (b.getType() != Material.AIR && b.getType() != Material.LONG_GRASS) continue;
+				
+				if (blocks.contains(b)) continue;
+				for (Player p : nearby) Util.sendFakeBlockChange(p, b, matNova);
+				blocks.add(b);
+			}
+			
+		}
+		
+		@Override
+		protected void stop() {
+			super.stop();
+			for (Block b : blocks) {
+				for (Player p : nearby) Util.restoreFakeBlockChange(p, b);
+			}
+			blocks.clear();
+		}
+		
+	}
+	
 }
